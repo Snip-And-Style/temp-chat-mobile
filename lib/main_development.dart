5d55353baaf4b272c59314c92bd747a8ed96fa24
Rostyslav Ulynets <poct2002@gmail.com>
2023-12-14 19:04:06 +0200

    Add Firebase configuration for Android and iOS

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,6 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 import 'package:snip_and_style/bootstrap.dart';
+import 'package:snip_and_style/firebase_options.dart';
 import 'package:snip_and_style/presentation/page/app.dart';
 
-void main() {
-  bootstrap(App.new);
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await bootstrap(App.new);
 }
