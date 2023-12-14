2f9b7283d0c0865888b3713804500660c4905faa
Rostyslav Ulynets <poct2002@gmail.com>
2023-12-14 21:36:52 +0200

    Add environment variables and update Firebase initialization

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,13 +1,21 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:snip_and_style/bootstrap.dart';
 import 'package:snip_and_style/firebase_options.dart';
 import 'package:snip_and_style/presentation/page/app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load();
   await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
+    options: FirebaseOptions(
+      apiKey: dotenv.env['API_KEY'].toString(),
+      appId: dotenv.env['APP_ID'].toString(),
+      messagingSenderId: dotenv.env['MESSAGING_SENDER_ID'].toString(),
+      projectId: dotenv.env['PROJECT_ID'].toString(),
+      storageBucket: dotenv.env['STORAGE_BUCKET'].toString(),
+    ),
   );
   await bootstrap(App.new);
 }
