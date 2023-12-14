8de9d5e4eae331305c84dd9e10ccebbc2bc317ac
Rostyslav Ulynets <poct2002@gmail.com>
2023-12-14 21:42:08 +0200

    Remove unused import statement

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -2,7 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:snip_and_style/bootstrap.dart';
-import 'package:snip_and_style/firebase_options.dart';
 import 'package:snip_and_style/presentation/page/app.dart';
 
 void main() async {
