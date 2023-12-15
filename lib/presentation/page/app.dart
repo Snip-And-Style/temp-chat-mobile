bf5567fd0ae949f460181c62c836584ba5860fc9
Rostyslav Ulynets <poct2002@gmail.com>
2023-12-15 14:58:06 +0200

    Add Google Fonts package and update color scheme

--- lib/presentation/page/app.dart
+++ lib/presentation/page/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:snip_and_style/config/l10n/l10n.dart';
 import 'package:snip_and_style/config/router/app_route.dart';
 
@@ -11,9 +12,21 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       theme: ThemeData(
-        appBarTheme: const AppBarTheme(color: Color(0xFF13B9FF)),
-        colorScheme: ColorScheme.fromSwatch(
-          accentColor: const Color(0xFF13B9FF),
+        colorScheme: const ColorScheme(
+          brightness: Brightness.light,
+          primary: Color(0xff374ACB),
+          onPrimary: Color(0xff060606),
+          secondary: Color(0xffC8DDF6),
+          onSecondary: Color(0xff0B0B0C),
+          error: Color(0xffBF2828),
+          onError: Color(0xff0B0B0C),
+          background: Color(0xffF8F9FB),
+          onBackground: Color(0xff0B0B0C),
+          surface: Color(0xffE7ECF2),
+          onSurface: Color(0xff0B0B0C),
+        ),
+        textTheme: GoogleFonts.latoTextTheme(
+          Theme.of(context).textTheme,
         ),
       ),
       localizationsDelegates: AppLocalizations.localizationsDelegates,
