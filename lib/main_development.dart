46ce81858ad408715ed2e8666faaa8873000ea52
Rostyslav Ulynets <poct2002@gmail.com>
2023-12-15 14:01:33 +0200

    Add GoogleService-Info.plist for iOS

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,20 +1,30 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart'
+    show TargetPlatform, defaultTargetPlatform;
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:snip_and_style/bootstrap.dart';
 import 'package:snip_and_style/presentation/page/app.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load();
   await Firebase.initializeApp(
-    options: FirebaseOptions(
-      apiKey: dotenv.env['API_KEY'].toString(),
-      appId: dotenv.env['APP_ID'].toString(),
-      messagingSenderId: dotenv.env['MESSAGING_SENDER_ID'].toString(),
-      projectId: dotenv.env['PROJECT_ID'].toString(),
-      storageBucket: dotenv.env['STORAGE_BUCKET'].toString(),
-    ),
+    options: getFirebaseOptions(),
   );
   await bootstrap(App.new);
+}
+
+FirebaseOptions getFirebaseOptions() {
+  final isAndroid = defaultTargetPlatform == TargetPlatform.android;
+
+  return FirebaseOptions(
+    apiKey:
+        dotenv.env[isAndroid ? 'ANDROID_API_KEY' : 'IOS_API_KEY'].toString(),
+    appId: dotenv.env[isAndroid ? 'ANDROID_APP_ID' : 'IOS_APP_ID'].toString(),
+    messagingSenderId: dotenv.env['MESSAGING_SENDER_ID'].toString(),
+    projectId: dotenv.env['PROJECT_ID'].toString(),
+    storageBucket: dotenv.env['STORAGE_BUCKET'].toString(),
+    iosBundleId: isAndroid ? null : dotenv.env['IOS_BUNDLE_ID'].toString(),
+  );
 }
