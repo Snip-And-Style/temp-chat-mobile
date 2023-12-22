80f82a17597241b505551ac526531bfd85465d7b
Rostyslav Ulynets <poct2002@gmail.com>
2023-12-22 10:01:37 +0200

    Refactor register page code

--- lib/presentation/page/auth/register/register_page.dart
+++ lib/presentation/page/auth/register/register_page.dart
@@ -1,8 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:snip_and_style/config/extensions/build_context_extension.dart';
-
-import 'package:snip_and_style/config/router/app_route.dart';
 import 'package:snip_and_style/presentation/page/auth/register/widgets/register_form.dart';
 
 const _verticalPadding = 50.0;
@@ -27,9 +25,7 @@ class RegisterPage extends StatelessWidget {
           child: Column(
             children: [
               _buildHeaderText(context),
-              _buildAnimatedLoginForm(),
-              // const Spacer(),
-              // _buildTextButton(context),
+              _buildAnimatedRegisterForm(),
             ],
           ),
         ),
@@ -53,35 +49,11 @@ class RegisterPage extends StatelessWidget {
     );
   }
 
-  Widget _buildAnimatedLoginForm() {
+  Widget _buildAnimatedRegisterForm() {
     return const AnimatedOpacity(
       duration: _animationDuration,
       opacity: 1,
       child: RegisterForm(),
-    );
-  }
-
-  Widget _buildTextButton(BuildContext context) {
-    return TextButton(
-      onPressed: () {
-        context.router.push(const RegisterRoute());
-      },
-      child: RichText(
-        text: TextSpan(
-          children: [
-            TextSpan(
-              text: context.l10n.newMember,
-              style: context.textTheme.bodyMedium,
-            ),
-            TextSpan(
-              text: context.l10n.registerNow,
-              style: context.textTheme.bodyMedium!.copyWith(
-                color: context.color.outlineVariant,
-              ),
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
