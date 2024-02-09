880fdfc0635df78992890aaa5036da0453cb63bf
Rostyslav Ulynets <poct2002@gmail.com>
2024-02-09 09:03:35 +0200

    CHAT-4 Update app theme

---
+++ lib/config/theme/text_theme.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class TempChatTextTheme {
+  static TextTheme get textTheme => TextTheme(
+        headlineLarge: TextStyle(
+          color: const Color(0xff060606),
+          fontFamily: GoogleFonts.lato().fontFamily,
+          fontWeight: FontWeight.w400,
+          fontSize: 36,
+        ),
+        headlineMedium: TextStyle(
+          color: const Color(0xff060606),
+          fontFamily: GoogleFonts.lato().fontFamily,
+          fontWeight: FontWeight.w400,
+          fontSize: 32,
+        ),
+        headlineSmall: TextStyle(
+          color: const Color(0xff060606),
+          fontFamily: GoogleFonts.lato().fontFamily,
+          fontWeight: FontWeight.w400,
+          fontSize: 24,
+        ),
+        bodyLarge: TextStyle(
+          color: const Color(0xff060606),
+          fontFamily: GoogleFonts.lato().fontFamily,
+          fontWeight: FontWeight.w400,
+          fontSize: 18,
+        ),
+        bodyMedium: TextStyle(
+          color: const Color(0xff060606),
+          fontFamily: GoogleFonts.lato().fontFamily,
+          fontWeight: FontWeight.w400,
+          fontSize: 16,
+        ),
+        bodySmall: TextStyle(
+          color: const Color(0xff060606),
+          fontFamily: GoogleFonts.lato().fontFamily,
+          fontWeight: FontWeight.w400,
+          fontSize: 14,
+        ),
+      );
+}
